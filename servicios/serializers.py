d272dcc6869e1e98ba05b8a2ed48d8ab92b75d54
Andrey0108 <juan_ptorol@soy.sena.edu.co>
2025-04-03 13:01:06 -0500

    VentaServicios

--- servicios/serializers.py
+++ servicios/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Servicio
+from .models import Servicio, VentaServicio
 
 class ServicioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servicio
+        fields = '__all__'
+        
+class VentaServicioSerializer(serializers.ModelSerializer):
+    
+    # Relación por ID
+    servicio = serializers.PrimaryKeyRelatedField(queryset=Servicio.objects.all())  
+     
+    class Meta:
+        model = VentaServicio
         fields = '__all__'
